dc54bee85969c0065276919aa1d30dabc74c94e7
Aawesh <aawesh2000@gmail.com>
2022-05-28 10:54:36 +0530

    Updates

---
+++ precache-manifest.11a4c850936463be528f17a861640c1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bce821063f1f09f0cb05bfda10f23f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3826cbb1d2cf79e928e5",
+    "url": "/static/css/main.4b52b7ee.chunk.css"
+  },
+  {
+    "revision": "15a2fe9ad89c592aaf6f",
+    "url": "/static/js/2.68978b41.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.68978b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3826cbb1d2cf79e928e5",
+    "url": "/static/js/main.e3cebf1f.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
